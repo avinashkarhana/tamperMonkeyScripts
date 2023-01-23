068a03a897bd8760ca2e9c84b34a77d9589a00bd
Avinash Karhana <avinashkarhana1@gmail.com>
2023-01-22 22:01:05 -0500

    Update Youtube Download Button Replace.user.js

--- Youtube Download Button Replace/Youtube Download Button Replace.user.js
+++ Youtube Download Button Replace/Youtube Download Button Replace.user.js
@@ -6,7 +6,7 @@
 // @downloadURL  https://github.com/avinashkarhana/tamperMonkeyScripts/raw/main/Youtube%20Download%20Button%20Replace/Youtube%20Download%20Button%20Replace.user.js
 // @description  Replace the default download button with a download button that will download the video in different formats and qualities.
 // @author       Avinash Karhana
-// @match        *://*.youtube.com/watch?v=*
+// @match        *://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
@@ -34,7 +34,6 @@
 
       observer.observe(bodyList, config);
   };
-  CustomisedDownloadButton();
 
   function CustomisedDownloadButton() {
       var downloaderUrl = "https://apiyoutube.cc/?url=";
@@ -43,7 +42,11 @@
           if (document.querySelector("#flexible-item-buttons > ytd-download-button-renderer > ytd-button-renderer > yt-button-shape > button") != null) {
               document.querySelector("#flexible-item-buttons > ytd-download-button-renderer > ytd-button-renderer > yt-button-shape > button").onclick = () => {var videoUrl = window.window.location.href;window.open(downloaderUrl+videoUrl, "_blank")};
               clearInterval(intId);
+              console.clear();
+              console.log("Set Download!");
           }
       }
   }
+
+  CustomisedDownloadButton();
 })();
